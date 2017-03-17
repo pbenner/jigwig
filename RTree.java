04098a687d204b6bf0a919364c5213d185170e75
Philipp Benner <benner@molgen.mpg.de>
2017-03-17 16:22:13 +0100

    2017/03/17-16:22:13 (Linux gnosiophobie.molgen.mpg.de unknown)

--- RTree.java
+++ RTree.java
@@ -14,5 +14,50 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+
+/* -------------------------------------------------------------------------- */
+
 class RTree {
+    static final int MAGIC = 0x2468ace0;
+
+    long Magic;
+    long BlockSize;
+    long NItems;
+    long ChrIdxStart;
+    long BaseStart;
+    long ChrIdxEnd;
+    long BaseEnd;
+    long IdxSize;
+    long NItemsPerSlot;
+    long PtrIdxSize;
+    RVertex Root;
+
+    RTree() {
+        BlockSize     = 256;
+        NItemsPerSlot = 1024;
+    }
+
+    void Read(SeekableByteChannel channel) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(8*32 + 2*64);
+        // read header
+        channel.read(buffer);
+
+        Magic         = buffer.getInt () & 0xFFFFFFFFL;
+        BlockSize     = buffer.getInt () & 0xFFFFFFFFL;
+        NItems        = buffer.getLong() & 0xFFFFFFFFL;
+        ChrIdxStart   = buffer.getInt () & 0xFFFFFFFFL;
+        BaseStart     = buffer.getInt () & 0xFFFFFFFFL;
+        ChrIdxEnd     = buffer.getInt () & 0xFFFFFFFFL;
+        BaseEnd       = buffer.getInt () & 0xFFFFFFFFL;
+        IdxSize       = buffer.getLong() & 0xFFFFFFFFL;
+        NItemsPerSlot = buffer.getInt () & 0xFFFFFFFFL;
+        // padding
+        buffer.getInt();
+        // parse tree
+        Root = new RVertex();
+        Root.Read(channel);
+    }
 }
